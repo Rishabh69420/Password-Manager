b7d6d5d5b4246539f571651c43a4dda7eca356cc
Rishabh Chopde <65121903+Rishabh69420@users.noreply.github.com>
2021-02-13 19:54:04 +0400

    Tests done, will clean up later

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-console.log("does this work ah");
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const url = require("url");
+const fs = require("fs");
+
+let data = fs.readFileSync("help.json", {encoding: "utf-8"});
+
+// fs.writeFile("help1.json", `${data.slice(0, data.length - 1)}, "brhouh": "nijeaivn"}`, function(err){
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("it worked somehow")
+//     };
+// });
+
+console.log(JSON.parse(data));
+
+console.log("does this work");
 
 let win;
 
@@ -15,7 +29,6 @@ function CreateWindow() {
         slashes: true
     }));
 
-    win.webContents.openDevTools();
     win.on("closed", () => {
         win = null;
     })
