85a9007b2e9fd27d86dedeeb9feedfedf86f8ef4
Rishabh Chopde <65121903+Rishabh69420@users.noreply.github.com>
2021-02-16 15:07:50 +0400

    temp changes, will fix later

--- filehandler.js
+++ filehandler.js
@@ -1,5 +1,12 @@
-const fs = require("fs");
-
 function ReadData(){
-    let data = fs.readFileSync("help.json", {encoding: "utf-8"});
+    const fs = require("fs");
+    var crude = fs.readFileSync("help.json", {encoding: "utf-8"});
+    var data = JSON.parse(crude);
+    var data1 = "";
+    for (var i in data){
+        data1 += `Email: ${data[i].email}<br>`;
+        data1 += `Username: ${data[i].username}<br>`;
+        data1 += `Password: ${data[i].password}<br><br>`;
+    };
+    document.getElementById("lol").innerHTML = data1;
 };
